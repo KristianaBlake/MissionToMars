02692b5072366a423190dffa2d5374658df8e19e
Kristiana Blake <kristiana.blake@gmail.com>
2020-07-21 15:10:07 -0500

    background image finally reaches bottom of page

--- react-mission-to-mars/src/App.js
+++ react-mission-to-mars/src/App.js
@@ -8,44 +8,45 @@ import WeNeedYou from './img/WeNeedYou.jpg';
 function App() {
   return (
     <div className="App">
-    	<div className="container">
-            <header className="header"> <strong> Mission to Mars </strong> </header>
-            
-            <nav className="nav"> 
+        <div className="background-image">
+        	<div className="container">
+                <header className="header"> <strong> Mission to Mars </strong> </header>
                 
-                <a className="nav-links" href="TheRedPlanet.html"> The Red Planet </a>
-            
-                <a className="nav-links" href="LifeOnMars.html"> Life on Mars </a>
-            
-                <a className="nav-links" href="apply.html"> Apply </a> 
+                <nav className="nav"> 
+                    
+                    <a className="nav-links" href="TheRedPlanet.html"> The Red Planet </a>
                 
-                <a className="nav-links" href="sources.html"> Sources </a>
-            
-            </nav>
-            
-            <div className="photo">
-            
-                <img src={WeNeedYou} alt="WeNeedYou" /> 
-            
-            </div>
-            
-            <article>
+                    <a className="nav-links" href="LifeOnMars.html"> Life on Mars </a>
                 
-                <br/>
-                <br/> 
-                <br/>  
+                    <a className="nav-links" href="apply.html"> Apply </a> 
+                    
+                    <a className="nav-links" href="sources.html"> Sources </a>
                 
-                <h1> Greetings Earthlings </h1>
+                </nav>
+                
+                <div className="photo">
+                
+                    <img src={WeNeedYou} alt="WeNeedYou" /> 
+                
+                </div>
+                
+                <article>
+                    
+                    <br/>
+                    <br/> 
+                    <br/>  
+                    
+                    <h1> Greetings Earthlings </h1>
 
-                <p> We, your fellow Martians are inviting you on a trip of a lifetime. Well...a one way trip that is. </p>
- 
-                <p> Here on Mars, our small population is thriving and we need hard working, creative individuals like yourself to help our Martian civilization grow. </p>
+                    <p> We, your fellow Martians are inviting you on a trip of a lifetime. Well...a one way trip that is. </p>
+     
+                    <p> Here on Mars, our small population is thriving and we need hard working, creative individuals like yourself to help our Martian civilization grow. </p>
+                    
+                    <p> Click on the links above to learn about this once in a lifetime opportunity. We hope to see you soon. </p>
                 
-                <p> Click on the links above to learn about this once in a lifetime opportunity. We hope to see you soon. </p>
-            
-            </article>
-        </div>
-    
+                </article>
+            </div>
+        </div> 
     </div>
   );
 }
