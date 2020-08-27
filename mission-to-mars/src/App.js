f4d52e3e780e3dc004b6bafebb5d5bed60b507b9
Kristiana Blake <kristiana.blake@gmail.com>
2020-08-26 23:30:52 -0500

    changed id and classNames back to originals

--- mission-to-mars/src/App.js
+++ mission-to-mars/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import './App.css';
 import Home from './pages/Home';
 
+
 function App() {
   return (
-  	<Home/> 
+  	<Router>
+  		<Home/> 
+  	</Router> 
   );
 }
 
--- mission-to-mars/src/pages/Home.js
+++ mission-to-mars/src/pages/Home.js
@@ -4,42 +4,42 @@ class Home extends React.Component{
 	render(){
 		return(
 			<div>
-				<body id ="indexBody">
-					<div id="indexBackground">
-						<div id="indexContainer">
-				            <header id ="indexHeader"> <strong> Mission to Mars </strong> </header>
+				<body id ="index-body">
+					<div id="index-background">
+						<div id="index-container">
+				            <header id ="index-header"> <strong> Mission to Mars </strong> </header>
 				            
 				            <nav className="navigation"> 
 				                
-				                <a className="navLinks" href="theredplanet.html"> The Red Planet </a>
+				                <a className="nav-links" href="theredplanet.html"> The Red Planet </a>
 				            
-				                <a className="navLinks" href="lifeonmars.html"> Life on Mars </a>
+				                <a className="nav-links" href="lifeonmars.html"> Life on Mars </a>
 				            
-				                <a className="navLinks" href="apply.html"> Apply </a> 
+				                <a className="nav-links" href="apply.html"> Apply </a> 
 				                
-				                <a className="navLinks" href="sources.html"> Sources </a>
+				                <a className="nav-links" href="sources.html"> Sources </a>
 				            
 				            </nav>
 				            
 				            <div id="photo">
 				            
-				                <img id="indexImage" src="assets/images/WeNeedYou.jpg" alt="WeNeedYou"/> 
+				                <img id="index-image" src="assets/images/WeNeedYou.jpg" alt="WeNeedYou"/> 
 				            
 				            </div>
 				            
-				            <article id="indexArticle">
+				            <article id="index-article">
 				                
 				                <br/>
 				                <br/> 
 				                <br/>  
 				                
-				                <h1 id="indexTitle"> Greetings Earthlings </h1>
+				                <h1 id="index-title"> Greetings Earthlings </h1>
 
-				                <p className="indexP"> We, your fellow Martians are inviting you on a trip of a lifetime. Well...a one way trip that is. </p>
+				                <p className="index-p"> We, your fellow Martians are inviting you on a trip of a lifetime. Well...a one way trip that is. </p>
 				 
-				                <p className="indexP"> Here on Mars, our small population is thriving and we need hard working, creative individuals like yourself to help our Martian civilization grow. </p>
+				                <p className="index-p"> Here on Mars, our small population is thriving and we need hard working, creative individuals like yourself to help our Martian civilization grow. </p>
 				                
-				                <p className="indexP"> Click on the links above to learn about this once in a lifetime opportunity. We hope to see you soon. </p>
+				                <p className="index-p"> Click on the links above to learn about this once in a lifetime opportunity. We hope to see you soon. </p>
 				            
 				            </article>
 				    	</div>
