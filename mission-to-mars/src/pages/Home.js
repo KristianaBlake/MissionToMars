33eeca342546923e015b2f5d18e9560a4e49f9f9
Kristiana Blake <kristiana.blake@gmail.com>
2020-08-26 19:59:07 -0500

    Home.js is working and connected to App.js. Commence happy dance

--- mission-to-mars/src/pages/Home.js
+++ mission-to-mars/src/pages/Home.js
@@ -4,42 +4,42 @@ class Home extends React.Component{
 	render(){
 		return(
 			<div>
-				<body id ="index-body">
-					<div id="index-background">
-						<div id="index-container">
-				            <header id ="index-header"> <strong> Mission to Mars </strong> </header>
+				<body id ="indexBody">
+					<div id="indexBackground">
+						<div id="indexContainer">
+				            <header id ="indexHeader"> <strong> Mission to Mars </strong> </header>
 				            
-				            <nav class="navigation"> 
+				            <nav className="navigation"> 
 				                
-				                <a class="nav-links" href="theredplanet.html"> The Red Planet </a>
+				                <a className="nav-links" href="theredplanet.html"> The Red Planet </a>
 				            
-				                <a class="nav-links" href="lifeonmars.html"> Life on Mars </a>
+				                <a className="nav-links" href="lifeonmars.html"> Life on Mars </a>
 				            
-				                <a class="nav-links" href="apply.html"> Apply </a> 
+				                <a className="nav-links" href="apply.html"> Apply </a> 
 				                
-				                <a class="nav-links" href="sources.html"> Sources </a>
+				                <a className="nav-links" href="sources.html"> Sources </a>
 				            
 				            </nav>
 				            
 				            <div id="photo">
 				            
-				                <img id="index-image" src="images/WeNeedYou.jpg" alt="WeNeedYou"/> 
+				                <img id="indexImage" src="assets/images/WeNeedYou.jpg" alt="WeNeedYou"/> 
 				            
 				            </div>
 				            
-				            <article id="index-article">
+				            <article id="indexArticle">
 				                
 				                <br/>
 				                <br/> 
 				                <br/>  
 				                
-				                <h1 id="index-title"> Greetings Earthlings </h1>
+				                <h1 id="indexTitle"> Greetings Earthlings </h1>
 
-				                <p class="index-p"> We, your fellow Martians are inviting you on a trip of a lifetime. Well...a one way trip that is. </p>
+				                <p className="indexP"> We, your fellow Martians are inviting you on a trip of a lifetime. Well...a one way trip that is. </p>
 				 
-				                <p class="index-p"> Here on Mars, our small population is thriving and we need hard working, creative individuals like yourself to help our Martian civilization grow. </p>
+				                <p className="indexP"> Here on Mars, our small population is thriving and we need hard working, creative individuals like yourself to help our Martian civilization grow. </p>
 				                
-				                <p class="index-p"> Click on the links above to learn about this once in a lifetime opportunity. We hope to see you soon. </p>
+				                <p className="indexP"> Click on the links above to learn about this once in a lifetime opportunity. We hope to see you soon. </p>
 				            
 				            </article>
 				    	</div>
