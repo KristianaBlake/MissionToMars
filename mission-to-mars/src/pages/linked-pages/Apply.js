b98425471c917b326895471e927632188dd831f6
Kristiana Blake <kristiana.blake@gmail.com>
2020-10-26 16:58:03 -0500

    form looking more aligned

--- mission-to-mars/src/pages/linked-pages/Apply.js
+++ mission-to-mars/src/pages/linked-pages/Apply.js
@@ -7,31 +7,38 @@ class Apply extends React.Component{
 		return(
 			<div>
 				<body id="apply-background">
-					<div id="apply-container">
-						<header id="apply-header"> Apply </header>
-
-						<h1 id="apply-title"> Sign up for your one-way-trip to Mars! </h1>
-						<br/>
-						<br/>
-						<form id="apply-form">
-							<br/>
-									<label> First Name: </label>
-									<input class="input-label"/>
-				
-							<br/>
-							
-									<label> Last Name: </label>
-									<input class="input-label"/>
-							
-							<br/>
-								
-									<label> Email Address: </label>
-									<input class="input-label"/>
-						
-							<br/>
-						</form>  
-					</div>
-				</body>
+	    			<div id="apply-container">
+	        			<header id="apply-header"> Apply 
+	        			</header>
+	       					<h1 id="apply-title"> Sign up for your one-way-trip to Mars! 
+	       					</h1>
+		       				 <form>
+		            			<table id="apply-table" border="0" cellpadding="8" cellspacing="0">
+		               				<tr>
+					                    <td align="right" valign="top"> First Name: </td>
+					                    <td> <input type="text" size="20" name="first"/> </td>
+					                </tr>
+					                <tr>
+					                    <td align="right" valign="top"> Last Name: </td>
+					                    <td> <input type="text" size="20" name="last"/> </td>
+					                </tr>
+					                <tr>
+					                    <td align="right" valign="top"> Email Address </td>
+					                    <td> <input class="apply-input" type="text" size="20" name="last"/> </td>
+					                </tr>
+					                <tr>
+					                    <td align="right"> Gender: </td>
+					                    <td> 
+					                        <input class="apply-input" type="radio" name="size" value="small"/> Male
+					                        <br/> 
+					                        <input class="apply-input" type="radio" name="size" value="medium"/> Female
+					                        <br/>
+					                    </td>
+					                </tr>
+		            			</table>
+		       				 </form>
+		       			</div>  
+		       		</body>  
 			</div> 
 		)
 	}
